9befde14f50a810e677107cdb03f2cf2f408360d
monjurur <monjurur.roshid@efgt.eurofoods-bd.com>
2024-05-30 01:25:18 +0600

    data type

---
+++ api_development/varibale.py
@@ -0,0 +1,18 @@
+name = 'fahim'
+full_name = "monjurur"
+
+# list datatype
+
+friends = ['fahim','rakib','fakrul','korim']
+
+
+# dict Data type
+
+friend = ('fahim','rakib','fakrul')
+print(friend)
+
+# bool data type
+age = 20
+isAdult = age >22
+
+print(isAdult)
