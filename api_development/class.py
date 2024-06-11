09ab254884b8e7cbcc81aaea7343044ebb29517f
monjurur <md899042@gmail.com>
2024-06-11 17:12:31 +0600

    error

--- api_development/class.py
+++ api_development/class.py
@@ -1,8 +1,6 @@
 class family:
  
     def getFamily(self):
-        return f"Hello {self.name} you are {self.age} years old. and you are a {self.designation}"
+        return f"Hello {self.name} you are{self.age}  years old. and you are a {self.designation} in the family"
 
-
-obj1 = family()
-print(obj1.getFamily("Monjurur", 25, "Software Engineer"))
+print(family().getFamily("Fahim",25,"Son"))    
